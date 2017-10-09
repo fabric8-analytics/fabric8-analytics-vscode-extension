8a3c287e654d1b6d52b6cf19a3118a944762fd16
invinciblejai <mailjai.vardhan@gmail.com>
2017-10-09 15:44:11 +0530

    hand auth wrt to osio-auth extension
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/lspmodule.ts
+++ src/lspmodule.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 import * as path from 'path';
+import { authextension } from './authextension';
 
 export module lspmodule {
 
@@ -13,34 +14,37 @@ export module lspmodule {
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
 	// The debug options for the server
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
-    let lastTagged = context.globalState.get('lastTagged', '');
-    if(lastTagged) {
-        process.env['RECOMMENDER_API_TOKEN'] = lastTagged;
-    }
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    let serverOptions: ServerOptions = {
-        run : { module: serverModule, transport: TransportKind.ipc },
-        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-    }
-	
-    // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents 'plaintext','xml','json'
-        documentSelector: [],
-            synchronize: {
-                // Synchronize the setting section 'languageServerExample' to the server
-                configurationSection: 'languageServerExample',
-                // Notify the server about file changes to '.clientrc files contain in the workspace
-                fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
-            }
-    }
-	
-    // Create the language client and start the client.
-    let disposableLSp = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions).start();
 
-    return disposableLSp;
-	
+    authextension.authorize_f8_analytics(context, (data) => {
+      if(data){
+        // If the extension is launched in debug mode then the debug server options are used
+        // Otherwise the run options are used
+        let serverOptions: ServerOptions = {
+            run : { module: serverModule, transport: TransportKind.ipc },
+            debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+        }
+        // Options to control the language client
+        let clientOptions: LanguageClientOptions = {
+            // Register the server for plain text documents 'plaintext','xml','json'
+            documentSelector: [],
+                synchronize: {
+                    // Synchronize the setting section 'languageServerExample' to the server
+                    configurationSection: 'languageServerExample',
+                    // Notify the server about file changes to '.clientrc files contain in the workspace
+                    fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+                }
+        }
+
+        // Create the language client and start the client.
+        let disposableLSp = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions).start();
+
+        return disposableLSp;
+
+      } else {
+        vscode.window.showErrorMessage("Looks like you are not authorized, Trigger OSIO-AUTH to authorize");
+      }
+    });
+
     }
 
 }

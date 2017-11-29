2d61de1a5ae46862e8368ffe672127d32ec660e0
invinciblejai <mailjai.vardhan@gmail.com>
2017-11-29 15:49:06 +0530

    updates route
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/apiendpoint.ts
+++ src/apiendpoint.ts
@@ -8,5 +8,7 @@ export namespace Apiendpoint {
     export let OSIO_REFRESH_TOKEN = '';
     export const STACK_API_URL: string = "https://recommender.api.openshift.io/api/v1/stack-analyses";
     export const OSIO_AUTH_URL: string = "https://auth.openshift.io/api/token/refresh";
+    export const OSIO_ROUTE_URL: string = "https://recommender.api.openshift.io/";
+    export const STACK_REPORT_URL: string = "https://stack-analytics-report.openshift.io/";
 
 }

693761251590c2baabce5a7eff0ad38cfee1d3f7
invinciblejai <mailjai.vardhan@gmail.com>
2018-01-19 16:23:06 +0530

    3scale integration
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/apiendpoint.ts
+++ src/apiendpoint.ts
@@ -10,7 +10,7 @@ export namespace Apiendpoint {
     export let STACK_API_URL: string = "";
     export let OSIO_ROUTE_URL: string = "";
     export const OSIO_AUTH_URL: string = "https://auth.openshift.io/api/token/refresh";
-    export const STACK_REPORT_URL: string = "https://stack-analytics-report.prod-preview.openshift.io/";
+    export const STACK_REPORT_URL: string = "https://stack-analytics-report.openshift.io/";
     export const THREE_SCALE_CONNECT_URL: string = "https://3scale-connect.api.prod-preview.openshift.io/";
 
 }

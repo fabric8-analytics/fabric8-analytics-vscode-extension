ebd800569e94a7a408b8dfda78d5b7151eb4f439
invinciblejai <mailjai.vardhan@gmail.com>
2018-01-19 18:00:47 +0530

    updates lsp release path
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- fetch_lsp.sh
+++ fetch_lsp.sh
@@ -1,8 +1,8 @@
 rm -rf ca-lsp-server.tar
 rm -rf ca-lsp-server
 
-#wget https://github.com/fabric8-analytics/fabric8-analytics-lsp-server/releases/download/0.0.5/ca-lsp-server.tar
-wget https://github.com/invincibleJai/sampleBootstrapTemplate/releases/download/0.0.6/ca-lsp-server.tar
+wget https://github.com/fabric8-analytics/fabric8-analytics-lsp-server/releases/download/0.0.7/ca-lsp-server.tar
+#wget https://github.com/invincibleJai/sampleBootstrapTemplate/releases/download/0.0.6/ca-lsp-server.tar
 
 archive=ca-lsp-server.tar
 mkdir ${archive%.tar*} 

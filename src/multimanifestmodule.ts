7bc1a4dda7a3d17903d8be1ece1a4a0692c7c28c
invinciblejai <mailjai.vardhan@gmail.com>
2018-01-22 14:55:44 +0530

    updates endpoint name, handles custom msg for 403
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/multimanifestmodule.ts
+++ src/multimanifestmodule.ts
@@ -29,7 +29,7 @@ export module multimanifestmodule {
                             const options = {};
                             let thatContext: any;
                             let file_uri: string;
-                            options['uri'] = `${Apiendpoint.STACK_API_URL}analyse?user_key=${Apiendpoint.STACK_API_USER_KEY}`;
+                            options['uri'] = `${Apiendpoint.STACK_API_URL}stack-analyses?user_key=${Apiendpoint.STACK_API_USER_KEY}`;
                             options['headers'] = {'Authorization': 'Bearer ' + OSIO_ACCESS_TOKEN};
                             options['formData'] = payloadData;
                             thatContext = context;
@@ -66,7 +66,7 @@ export module multimanifestmodule {
                             const options = {};
                             let thatContext: any;
                             let file_uri: string;
-                            options['uri'] = `${Apiendpoint.STACK_API_URL}analyse?user_key=${Apiendpoint.STACK_API_USER_KEY}`;
+                            options['uri'] = `${Apiendpoint.STACK_API_URL}stack-analyses?user_key=${Apiendpoint.STACK_API_USER_KEY}`;
                             options['headers'] = {'Authorization': 'Bearer ' + OSIO_ACCESS_TOKEN};
                             options['formData'] = payloadData;
                             thatContext = context;

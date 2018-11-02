c6c9f21b4337a662a5dec50d2fbd3c467e1b6d38
invinciblejai <mailjai.vardhan@gmail.com>
2018-11-02 13:49:34 +0530

    fix(code-action): feature to support qucikfixes with codeaction

--- src/commands.ts
+++ src/commands.ts
@@ -9,6 +9,7 @@ export namespace Commands {
      */
     export const TRIGGER_STACK_ANALYSIS = 'extension.fabric8AnalyticsWidget';
     export const TRIGGER_FULL_STACK_ANALYSIS = 'extension.fabric8AnalyticsWidgetFullStack';
+    export const TRIGGER_LSP_EDIT = 'lsp.applyTextEdit';
 
 }
 

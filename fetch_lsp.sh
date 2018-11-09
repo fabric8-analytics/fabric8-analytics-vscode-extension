2b41b83d97586db36ca6b8257b813f1756ba4e14
invinciblejai <mailjai.vardhan@gmail.com>
2018-11-09 15:21:55 +0530

    fix(info): updates info popup to show only if CVEs are identified and count changes on edit

--- fetch_lsp.sh
+++
@@ -1,9 +0,0 @@
-rm -rf ca-lsp-server.tar
-rm -rf ca-lsp-server
-
-wget https://github.com/fabric8-analytics/fabric8-analytics-lsp-server/releases/download/0.0.7/ca-lsp-server.tar
-#wget https://github.com/invincibleJai/sampleBootstrapTemplate/releases/download/0.0.6/ca-lsp-server.tar
-
-archive=ca-lsp-server.tar
-mkdir ${archive%.tar*} 
-tar --extract --file=${archive} --strip-components=1 --directory=${archive%.tar*} 

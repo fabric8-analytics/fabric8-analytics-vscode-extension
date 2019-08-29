5a6f83a3cdbbd6110e8193a0133a85f26a8731e4
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2019-08-29 12:33:33 +0530

    fix(timeout): Fix stack report polling frequency and duration

--- src/constants.ts
+++ src/constants.ts
@@ -12,3 +12,7 @@ export enum GlobalState {
 export const extensionId = 'fabric8-analytics';
 // publisher.name from package.json
 export const extensionQualifiedId = `redhat.${extensionId}`;
+// GET request timeout
+export const getRequestTimeout = 20 * 1000; // ms
+// GET request polling frequency
+export const getRequestPollInterval = 2 * 1000; // ms

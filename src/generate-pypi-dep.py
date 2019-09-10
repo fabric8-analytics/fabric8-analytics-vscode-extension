f028147fbfe305ebedc151603295c69fcffc99e5
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2019-09-10 23:23:25 +0530

    Beautify dependency generator script

--- src/generate-pypi-dep.py
+++ src/generate-pypi-dep.py
@@ -1,12 +1,29 @@
-import pkg_resources as pr;import json,sys;gd=pr.get_distribution;res=list();
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pkg_resources as pr
+import json
+import sys
+
+gd = pr.get_distribution
+result = list()
+
 for i in open(sys.argv[1]):
     try:
-        rs={};I=gd(i);rs["package"]=I.key;rs["version"]=I.version;rs["deps"]=set();
+        # get direct dependency details
+        direct_dep = gd(i)
+        rs = {'package': direct_dep.key, 'version': direct_dep.version}
+        rs['deps'] = set()
         for j in pr.require(i):
             for k in j.requires():
-                K=gd(k);rs["deps"].add((K.key, K.version))
-        rs["deps"]=[{"package":p,"version":v}for p,v in rs["deps"]];res.append(rs)
-    except: pass
-a=sys.argv[2:3]
-op=open(a[0],"w")if a else sys.stdout
-json.dump(res,op)
+                # get transitive dependency details
+                transitive_dep = gd(k)
+                rs['deps'].add((transitive_dep.key, transitive_dep.version))
+        rs['deps'] = [{'package': p, 'version': v} for (p, v) in rs['deps']]
+        result.append(rs)
+    except:
+        pass
+
+output_file = sys.argv[2:3]
+output_file_obj = open(output_file[0], 'w') if output_file else sys.stdout
+json.dump(result, output_file_obj)

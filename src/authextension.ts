523dc0d8c073b391a1e35c3b4232b70434093ec5
Darshan Vandra <dvandra.ai@gmail.com>
2020-09-10 18:44:22 +0530

    feat: Add anonymous user management/tracking (#406)

--- src/authextension.ts
+++ src/authextension.ts
@@ -2,6 +2,8 @@
 
 import { Apiendpoint } from './apiendpoint';
 import { stackAnalysisServices } from './stackAnalysisService';
+import { GlobalState } from './constants';
+import * as fetch from 'node-fetch'
 
 export module authextension {
   export let setContextData: any;
@@ -15,28 +17,62 @@ export module authextension {
     process.env['THREE_SCALE_USER_TOKEN'] = context_f8_3scale_user_key;
   };
 
-  export const authorize_f8_analytics = context => {
-    return new Promise((resolve, reject) => {
+  export function setUUID(uuid) {
+    process.env['UUID'] = uuid;
+  };
+
+  export const authorize_f8_analytics = async context => {
+    try {
       let context_f8_access_routes = context.globalState.get(
         'f8_access_routes'
       );
       let context_f8_3scale_user_key = context.globalState.get(
         'f8_3scale_user_key'
       );
+
       if (context_f8_access_routes && context_f8_3scale_user_key) {
         setContextData(context_f8_access_routes, context_f8_3scale_user_key);
-        resolve(true);
       } else {
-        get_3scale_routes(context)
-          .then(resp => {
-            resolve(resp);
-          })
-          .catch(err => {
-            reject(err);
-          });
+        let respData = await get_3scale_routes(context);
+        if (!respData) {
+          return false;
+        }
       }
-    });
+
+      let uuid = context.globalState.get(GlobalState.UUID);
+
+      if (uuid && uuid != '') {
+        setUUID(uuid);
+      } else {
+        uuid = await getUUID();
+        if (uuid) {
+          context.globalState.update(GlobalState.UUID, uuid);
+          setUUID(uuid);
+        } else {
+          return false;
+        }
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   };
+
+  export async function getUUID(): Promise<string> {
+    const url = `${
+      Apiendpoint.OSIO_ROUTE_URL
+      }/user?user_key=${Apiendpoint.STACK_API_USER_KEY}`;
+
+    const response = await fetch(url, { method: 'POST' });
+    if (response.ok) {
+      let respData = await response.json();
+      return respData['user_id'];
+    } else {
+      throw (`${url} : ` + response.status);
+    }
+  }
 
   export const get_3scale_routes = context => {
     return new Promise((resolve, reject) => {

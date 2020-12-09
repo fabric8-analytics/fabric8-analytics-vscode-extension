b68f074714765ca6d61884225f32c0686a811b00
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-12-09 15:19:25 +0530

    fix: Assign defaults for all props from constructor

--- src/caNotification.ts
+++ src/caNotification.ts
@@ -2,35 +2,49 @@
 
 class CANotification {
 
-  private respData: any;
+  private diagCount: number;
+  private done: boolean;
+  private vulnerabilityCount: number;
+  private advisoryCount: number;
+  private exploitCount: number;
+  private depCount: number;
+  private uri: string;
+  private text: string;
   constructor(respData: any) {
-    this.respData = respData;
+    this.diagCount = respData.diagCount || 0;
+    this.done = respData.done === true;
+    const vulnCount = respData.vulnCount || {vulnerabilityCount: 0, advisoryCount: 0, exploitCount: 0};
+    this.vulnerabilityCount = vulnCount.vulnerabilityCount;
+    this.advisoryCount = vulnCount.advisoryCount;
+    this.exploitCount = vulnCount.exploitCount;
+    this.depCount = respData.depCount || 0;
+    this.uri = respData.uri;
+    this.text = respData.data;
   }
 
   public origin(): string {
-    return this.respData.uri;
+    return this.uri;
   }
 
   public isDone(): boolean {
-    return this.respData.done === true;
+    return this.done;
   }
 
   public hasWarning(): boolean {
-    return (this.respData.diagCount || 0) > 0;
+    return this.diagCount > 0;
   }
 
   public popupText(): string {
-    return this.respData.data;
+    return this.text;
   }
 
   private vulnCountText(): string {
-    const vulns = this.respData.vulnCount.vulnerabilityCount || 0 + this.respData.vulnCount.advisoryCount || 0;
+    const vulns = this.vulnerabilityCount + this.advisoryCount;
     return vulns > 0 ? `${vulns} vulns` : ``;
   }
 
   private exploitCountText(): string {
-    const exploits = this.respData.vulnCount.exploitCount || 0;
-    return exploits > 0 ? `${exploits} exploits` : ``;
+    return this.exploitCount > 0 ? `${this.exploitCount} exploits` : ``;
   }
 
   public statusText(): string {
@@ -39,9 +53,9 @@ class CANotification {
     }
     if (this.hasWarning()) {
       const finalStatus = [this.vulnCountText(), this.exploitCountText()].filter(t => t.length > 0).join(`, `);
-      return `$(warning) ${finalStatus} found in ${this.respData.depCount} deps`;
+      return `$(warning) ${finalStatus} found in ${this.depCount} deps`;
     }
-    return `$(thumbsup) no vulns found in ${this.respData.depCount} deps`;
+    return `$(thumbsup) no vulns found in ${this.depCount} deps`;
   }
 }
 

e8775535c7982f66047d6398fe5d11852e7c94a9
Shubham Mathur <shumathu@redhat.com>
2021-01-18 20:50:16 +0530

    fix not required changes

--- src/extension.ts
+++ src/extension.ts
@@ -10,11 +10,7 @@ import {
 import * as path from 'path';
 
 import { Commands } from './commands';
-import {
-  GlobalState,
-  extensionQualifiedId,
-  registrationURL,
-} from './constants';
+import { GlobalState, extensionQualifiedId, registrationURL } from './constants';
 import { multimanifestmodule } from './multimanifestmodule';
 import { authextension } from './authextension';
 import { StatusMessages } from './statusMessages';
@@ -53,12 +49,14 @@ export function activate(context: vscode.ExtensionContext) {
   // show welcome message after first install or upgrade
   showUpdateNotification(context);
 
-  authextension.authorize_f8_analytics(context).then((data) => {
+  authextension.authorize_f8_analytics(context).then(data => {
     if (data) {
       // Create output channel
       outputChannelDep = initOutputChannel();
       // The server is implemented in node
-      let serverModule = context.asAbsolutePath(path.join('dist', 'server.js'));
+      let serverModule = context.asAbsolutePath(
+        path.join('dist', 'server.js')
+      );
       // The debug options for the server
       // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
       let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
@@ -70,7 +68,7 @@ export function activate(context: vscode.ExtensionContext) {
         debug: {
           module: serverModule,
           transport: TransportKind.ipc,
-          options: debugOptions,
+          options: debugOptions
         },
       };
 
@@ -83,14 +81,14 @@ export function activate(context: vscode.ExtensionContext) {
           { scheme: 'file', language: 'plaintext' },
           { scheme: 'file', language: 'pip-requirements' },
           { scheme: 'file', language: 'go' },
-          { scheme: 'file', language: 'go.mod' },
+          { scheme: 'file', language: 'go.mod' }
         ],
         synchronize: {
           // Synchronize the setting section 'dependencyAnalyticsServer' to the server
           configurationSection: 'dependencyAnalyticsServer',
           // Notify the server about file changes to '.clientrc files contained in the workspace
-          fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
-        },
+          fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+        }
       };
 
       // Create the language client and start the client.
@@ -109,23 +107,15 @@ export function activate(context: vscode.ExtensionContext) {
         };
 
         const showVulnerabilityFoundPrompt = async (msg: string) => {
-          const selection = await vscode.window.showWarningMessage(
-            `${msg}. Powered by [Snyk](${registrationURL})`,
-            StatusMessages.FULL_STACK_PROMPT_TEXT
-          );
+          const selection = await vscode.window.showWarningMessage(`${msg}. Powered by [Snyk](${registrationURL})`, StatusMessages.FULL_STACK_PROMPT_TEXT);
           if (selection === StatusMessages.FULL_STACK_PROMPT_TEXT) {
-            vscode.commands.executeCommand(
-              Commands.TRIGGER_FULL_STACK_ANALYSIS
-            );
+            vscode.commands.executeCommand(Commands.TRIGGER_FULL_STACK_ANALYSIS);
           }
         };
 
         lspClient.onNotification('caNotification', (respData) => {
           const notification = new CANotification(respData);
-          caStatusBarProvider.showSummary(
-            notification.statusText(),
-            notification.origin()
-          );
+          caStatusBarProvider.showSummary(notification.statusText(), notification.origin());
           if (canShowPopup(notification)) {
             showVulnerabilityFoundPrompt(notification.popupText());
             // prevent further popups.
@@ -133,7 +123,7 @@ export function activate(context: vscode.ExtensionContext) {
           }
         });
 
-        lspClient.onNotification('caError', (respData) => {
+        lspClient.onNotification('caError', respData => {
           const notification = new CANotification(respData);
           caStatusBarProvider.setError();
           vscode.window.showErrorMessage(respData.data);
@@ -143,7 +133,7 @@ export function activate(context: vscode.ExtensionContext) {
         lspClient.start(),
         disposableFullStack,
         disposableStackLogs,
-        caStatusBarProvider
+        caStatusBarProvider,
       );
     }
   });
@@ -163,20 +153,17 @@ export function deactivate(): Thenable<void> {
 
 async function showUpdateNotification(context: vscode.ExtensionContext) {
   // Retrive current and previous version string to show welcome message
-  const packageJSON = vscode.extensions.getExtension(extensionQualifiedId)
-    .packageJSON;
+  const packageJSON = vscode.extensions.getExtension(extensionQualifiedId).packageJSON;
   const version = packageJSON.version;
   const previousVersion = context.globalState.get<string>(GlobalState.Version);
   // Nothing to display
-  if (version === previousVersion) return;
+  if (version === previousVersion) 
+    return;
 
   // store current version into localStorage
   context.globalState.update(GlobalState.Version, version);
 
-  const actions: vscode.MessageItem[] = [
-    { title: 'README' },
-    { title: 'Release Notes' },
-  ];
+  const actions: vscode.MessageItem[] = [{ title: 'README' }, { title: 'Release Notes' }];
 
   const displayName = packageJSON.displayName;
   const result = await vscode.window.showInformationMessage(
@@ -186,17 +173,9 @@ async function showUpdateNotification(context: vscode.ExtensionContext) {
 
   if (result != null) {
     if (result === actions[0]) {
-      await vscode.commands.executeCommand(
-        'vscode.open',
-        vscode.Uri.parse(packageJSON.homepage)
-      );
+      await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(packageJSON.homepage));
     } else if (result === actions[1]) {
-      await vscode.commands.executeCommand(
-        'vscode.open',
-        vscode.Uri.parse(
-          `${packageJSON.repository.url}/releases/tag/${version}`
-        )
-      );
+      await vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${packageJSON.repository.url}/releases/tag/${version}`));
     }
   }
 }

f6a02b36642614311ec001d45641a4557c68719f
Shubham Mathur <shumathu@redhat.com>
2021-02-03 17:24:43 +0530

    add enum

--- src/redhatTelemetry.ts
+++ src/redhatTelemetry.ts
@@ -1,9 +1,16 @@
 import { getTelemetryService, TelemetryEvent, TelemetryService } from "@redhat-developer/vscode-redhat-telemetry";
 
+enum ActionType {
+  Identify = 'identify',
+  Track = 'track',
+  Page = 'page',
+  Group = 'group',
+}
+
 export async function record(eventName: string, properties?: object) {
   const telemetryService: TelemetryService = await getTelemetryService("redhat.fabric8-analytics")
   let event:TelemetryEvent={
-    type: 'track',
+    type: ActionType.Track,
     name: eventName,
     properties: properties
   }

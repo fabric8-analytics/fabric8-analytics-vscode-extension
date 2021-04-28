f44b29d61c7db318b9f5b32f7c8ae9f5c759a2ee
Shubham Mathur <shumathu@redhat.com>
2021-04-28 13:21:49 +0530

    update prefix

--- src/commands.ts
+++ src/commands.ts
@@ -7,15 +7,15 @@ export namespace Commands {
   /**
    * Triggers Stack Analysis
    */
-  export const TRIGGER_FULL_STACK_ANALYSIS = 'extension.stackAnalysis';
+  export const TRIGGER_FULL_STACK_ANALYSIS = 'fabric8.stackAnalysis';
   export const TRIGGER_FULL_STACK_ANALYSIS_FROM_STATUS_BAR =
-    'extension.stackAnalysisFromStatusBar';
+    'fabric8.stackAnalysisFromStatusBar';
   export const TRIGGER_FULL_STACK_ANALYSIS_FROM_EXPLORER =
-    'extension.stackAnalysisFromExplorer';
+    'fabric8.stackAnalysisFromExplorer';
   export const TRIGGER_FULL_STACK_ANALYSIS_FROM_PIE_BTN =
-    'extension.stackAnalysisFromPieBtn';
+    'fabric8.stackAnalysisFromPieBtn';
   export const TRIGGER_FULL_STACK_ANALYSIS_FROM_EDITOR =
-    'extension.stackAnalysisFromEditor';
+    'fabric8.stackAnalysisFromEditor';
   export const TRIGGER_LSP_EDIT = 'lsp.applyTextEdit';
-  export const TRIGGER_STACK_LOGS = 'extension.fabric8AnalyticsStackLogs';
+  export const TRIGGER_STACK_LOGS = 'fabric8.fabric8AnalyticsStackLogs';
 }

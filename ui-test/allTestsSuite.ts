813336934298bd48e21424f59fe2ad685748f4b4
Manish Bainsla <mbainsla@redhat.com>
2021-05-31 14:44:26 +0530

    removed definitions from allTestSuite.ts

--- ui-test/allTestsSuite.ts
+++ ui-test/allTestsSuite.ts
@@ -2,21 +2,13 @@ import { VSBrowser, WebDriver, SideBarView, ActivityBar, EditorView, By, WebView
 import { expect } from 'chai';
 import { addFolderToWorkspace, removeFolderFromWorkspace } from "./common/commonUtils";
 let path = require('path');
-import { pypiWithVulnsUITest } from './manifestFileTests/pypiWithVulns';
-import { pypiWithoutVulnsUITest } from './manifestFileTests/pypiWithoutVulns';
-import { npmWithVulnsUITest } from './manifestFileTests/npmWithVulns';
-import { npmWithoutVulnsUITest } from "./manifestFileTests/npmWithoutVulns";
 var fs = require('fs');
 let os = require('os');
 import debug from 'debug'
 const log = debug('server');
-import { delay } from "./common/helperUtils";
-import { goWithoutVulnsUITest } from "./manifestFileTests/goWithoutVulns";
-import { mavenWithoutVulnsUITest } from "./manifestFileTests/mavenWithoutVulns";
-import { goWithVulnsUITest } from "./manifestFileTests/goWithVulns";
-import { mavenWithVulnsUITest } from "./manifestFileTests/mavenWithVulns";
 import { checkPIEBTN, checkStatusBarBTN } from "./common/negativeTests";
 import { testWithoutVulns, testWithVulns } from "./common/mainTestUtils";
+import { changeWorkspace, checkWorkspaceSection } from "./common/workspaceUtils";
 var assert = require('assert');
 
 describe('UI tests', function () {
@@ -31,36 +23,15 @@ describe('UI tests', function () {
         addFolderToWorkspace(homedir);
     });
 
-    describe('checking workspace section', function () {
-        it('workspace sections should not be empty', async function () {
-            this.timeout(5000);
-            const sections = new SideBarView().getContent().getSections();
-            expect(sections).not.to.be.null;
-        })
-    });
+    describe('checking workspace section', checkWorkspaceSection);
 
     describe('negative test 1: statusbar btn absent before opening manifest file', checkStatusBarBTN);
 
-    testWithVulns();
+    describe('test with vulns', testWithVulns);
 
-    describe('change workspace', function () {
-        it('remove manifests folder from workspace and add manifest1 folder', async function () {
-            dir = path.resolve("./manifests1");
-            homedir = dir
-            addFolderToWorkspace(homedir);
-            removeFolderFromWorkspace(path.resolve("./manifests"))
-        });
+    describe('change workspace', changeWorkspace);
 
-        it('workspace sections should not be empty', async function () {
-            this.timeout(5000);
-            const sections = new SideBarView().getContent().getSections();
-            expect(sections).not.to.be.null;
-        })
-
-        delay(2000);
-    });
-
-    testWithoutVulns();
+    describe('test without vulns', testWithoutVulns);
 
     describe('negative test 3 : PIE btn absent after closing manifest file', checkPIEBTN);
 

98bf0e0480161e4aab6742f363ddaae48f523d61
Manish Bainsla <mbainsla@redhat.com>
2021-06-04 15:29:18 +0530

    added go.sum

--- manifests1/main.go
+++ manifests1/main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// import _ "github.com/unknwon/cae"
-// import _ "github.com/unknwon/cae/zip"
+	// import _ "github.com/unknwon/cae"
+	// import _ "github.com/unknwon/cae/zip"
 
-import _ "code.cloudfoundry.org/archiver/extractor"
-import _ "github.com/googleapis/gax-go"
-import _ "github.com/googleapis/gax-go/v2"
+	_ "code.cloudfoundry.org/archiver/extractor"
+
+	_ "github.com/googleapis/gax-go"
+
+	_ "github.com/googleapis/gax-go/v2"
+)
 
 func main() {
 	fmt.Println("vim-go")

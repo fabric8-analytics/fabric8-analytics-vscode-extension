67231e173ceb886e7db89aa8ba784a9a159f72e6
ruromero <rromerom@redhat.com>
2023-06-27 14:16:11 +0200

    fetch
    Signed-off-by: ruromero <rromerom@redhat.com>

--- src/authextension.ts
+++ src/authextension.ts
@@ -3,7 +3,6 @@
 import { GlobalState } from './constants';
 import { Config } from './config';
 import { getRedHatService } from '@redhat-developer/vscode-redhat-telemetry/lib';
-import fetch from 'node-fetch';
 
 export module authextension {
   const apiConfig = Config.getApiConfig();

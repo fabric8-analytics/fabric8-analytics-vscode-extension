f840cfdf86d9980f847788b3d6e0d692ecba55a9
Ilona Shishov <Ilona.Shishov@gmail.com>
2023-08-03 15:44:00 +0300

    refactor: from crda to exhort
    Signed-off-by: Ilona Shishov <Ilona.Shishov@gmail.com>

--- src/authextension.ts
+++ src/authextension.ts
@@ -14,7 +14,7 @@ export module authextension {
     process.env['PROVIDE_FULLSTACK_ACTION'] = 'true';
     process.env['GOLANG_EXECUTABLE'] = Config.getGoExecutable();
     process.env['UTM_SOURCE'] = 'vscode';
-    process.env['CRDA_SNYK_TOKEN'] = apiConfig.crdaSnykToken;
+    process.env['EXHORT_SNYK_TOKEN'] = apiConfig.exhortSnykToken;
   };
 
   export function setUUID(uuid) {

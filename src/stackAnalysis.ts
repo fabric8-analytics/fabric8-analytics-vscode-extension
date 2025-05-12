745c1c234acdcbaacb14218a30d13e4228f4fb84
Ruben Romero Montes <rromerom@redhat.com>
2025-05-12 13:32:50 +0200

    feat: add support for pnpm (#767)
    * feat: add support for pnpm
    
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>
    
    * build(ci): create .npmrc in the working dir
    
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>
    
    * build: update package-lock.json
    
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>
    
    ---------
    
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- src/stackAnalysis.ts
+++ src/stackAnalysis.ts
@@ -31,6 +31,7 @@ export async function executeStackAnalysis(manifestFilePath: string, outputChann
       'EXHORT_PREFER_MVNW': globalConfig.exhortPreferMvnw,
       'EXHORT_GRADLE_PATH': globalConfig.exhortGradlePath,
       'EXHORT_NPM_PATH': globalConfig.exhortNpmPath,
+      'EXHORT_PNPM_PATH': globalConfig.exhortPnpmPath,
       'EXHORT_GO_PATH': globalConfig.exhortGoPath,
       'EXHORT_PYTHON3_PATH': globalConfig.exhortPython3Path,
       'EXHORT_PIP3_PATH': globalConfig.exhortPip3Path,

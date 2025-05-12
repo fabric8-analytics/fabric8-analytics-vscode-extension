678281d330c7c05f5c6b5e7febaf5e4a69777740
Ruben Romero Montes <rromerom@redhat.com>
2025-05-12 13:05:26 +0200

    feat: add support for yarn
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- src/stackAnalysis.ts
+++ src/stackAnalysis.ts
@@ -32,6 +32,7 @@ export async function executeStackAnalysis(manifestFilePath: string, outputChann
       'EXHORT_GRADLE_PATH': globalConfig.exhortGradlePath,
       'EXHORT_NPM_PATH': globalConfig.exhortNpmPath,
       'EXHORT_PNPM_PATH': globalConfig.exhortPnpmPath,
+      'EXHORT_YARN_PATH': globalConfig.exhortYarnPath,
       'EXHORT_GO_PATH': globalConfig.exhortGoPath,
       'EXHORT_PYTHON3_PATH': globalConfig.exhortPython3Path,
       'EXHORT_PIP3_PATH': globalConfig.exhortPip3Path,

eee336be6257b3154b6db3e75ae0c97e6f4dbec0
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2025-06-09 12:06:47 +0100

    chore: testing test coverage

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+    files: 'out/test/**/*.test.js',
     version: '1.78.1',
     installExtensions: ['redhat.vscode-commons'],
-    launchArgs: ['--user-data-dir=/tmp/vscode-test']
+    launchArgs: ['--user-data-dir=/tmp/vscode-test'],
+    coverage: {
+        reporter: ['text', 'text-summary', 'lcov'],
+        includeAll: true,
+        exclude: ['exhortServices.js', 'exhortServices_rewire.js', 'redhatTelemetry.js', 'redhatTelemetry_rewire.js']
+    }
 });
